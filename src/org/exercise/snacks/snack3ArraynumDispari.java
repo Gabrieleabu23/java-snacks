5622f92e200f5f6679a86ed6e6e3d40c90070987
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-05-16 14:39:08 +0200

    fix: third snack

--- src/org/exercise/snacks/snack3ArraynumDispari.java
+++ src/org/exercise/snacks/snack3ArraynumDispari.java
@@ -9,7 +9,7 @@ public class snack3ArraynumDispari {
         int[] vetNumber = new int[rand.nextInt(5,30)];
         for (int i = 0; i <vetNumber.length ; i++) {
             vetNumber[i] = rand.nextInt(80);
-            if(vetNumber[i] % 2 !=0){
+            if(i % 2 !=0){
                 numDispari+=vetNumber[i];
             }
         }
